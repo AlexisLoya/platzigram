1b817ca583f49572682167bd072e7fee6671658a
AlexisLoya <alexloy117@gmail.com>
2021-04-30 17:13:06 -0500

    implement profile image view

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -20,11 +20,13 @@ from django.contrib import admin
 from django.urls import path
 from platzigram import views as local_views
 from posts import views as post_views
-
+#view media
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello-word/', local_views.hello_word),
     path('sorted/', local_views.sort_integers),
     path('hi/<str:name>/<int:age>', local_views.say_hi),
     path('posts/', post_views.list_post),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
